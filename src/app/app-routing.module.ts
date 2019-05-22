6e27e2f453059c1ebf31fcaa29b008e0e7bbd53f
Wevan <438864121@qq.com>
2019-05-22 09:53:19 +0800

    专业目标前端未完成

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', loadChildren: './user/user.module#UserModule' },
   { path: 'college', loadChildren: './college/college.module#CollegeModule' },
+  { path: 'collegeTarget', loadChildren: './college/college.module#CollegeModule' },
 ];
 
 @NgModule({
